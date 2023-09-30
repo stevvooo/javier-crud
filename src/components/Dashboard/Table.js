d1d1e39e02b79be74511279bbb398236bdbbad81
javier <stubacr@miumg.edu.gt>
2023-09-29 20:00:04 -0600

    first commit

---
+++ src/components/Dashboard/Table.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const Table = ({ employees, handleEdit, handleDelete }) => {
+  employees.forEach((employee, i) => {
+    employee.id = i + 1;
+  });
+
+  const formatter = new Intl.NumberFormat('es-US', {
+    style: 'currency',
+    currency: 'GTQ',
+    minimumFractionDigits: null,
+  });
+
+  return (
+    <div className="contain-table">
+      <table className="striped-table">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>NOMBRE</th>
+            <th>APELLIDOS</th>
+            <th>CORREO</th>
+            <th>SUELDO</th>
+            <th>FECHA</th>
+            <th colSpan={2} className="text-center">
+              ACCIONES
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.length > 0 ? (
+            employees.map((employee, i) => (
+              <tr key={employee.id}>
+                <td>{i + 1}</td>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.email}</td>
+                <td>{formatter.format(employee.salary)}</td>
+                <td>{employee.date} </td>
+                <td className="text-right">
+                  <button
+                    onClick={() => handleEdit(employee.id)}
+                    className="button muted-button"
+                  >
+                    EDITAR
+                  </button>
+                </td>
+                <td className="text-left">
+                  <button
+                    onClick={() => handleDelete(employee.id)}
+                    className="button muted-button"
+                  >
+                    ELIMINAR
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={7}>No Employees</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
